5bd5e28ea53ff4848d0690eae97bccbafde14736
Marco Kuan <mnkuan@dons.usfca.edu>
2019-02-15 09:53:26 -0800

    fix spacing

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,19 +1,20 @@
 
 public class BubbleSort implements SortingAlgorithm {
-  public void sort(int[] Arr) {
+  public void sort(int[] arr) {
     // Loop through each Array
-    for (int i = 0; i < Arr.length; i++) {
-      for (int j = 0; j < Arr.length - i - 1; j++) {
-        if (Arr[j] > Arr[j + 1]) {
-          swap(Arr, j, j + 1];
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1];
         }
       }
     }
   }
-  
-  private void swap(int[] Arr, int before, int after) {
-    int temp = Arr[before];
-    Arr[before] = Arr[after];
-    Arr[after] = temp;
+
+  // Swaps the values of two indexes of an array
+  private void swap(int[] arr, int before, int after) {
+    int temp = arr[before];
+    arr[before] = arr[after];
+    arr[after] = temp;
   }
 }
