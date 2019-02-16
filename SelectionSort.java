816236e220de1ecc39f8945e07ceb73a982bb18a
Marco Kuan <mnkuan@dons.usfca.edu>
2019-02-15 20:32:38 -0800

    fix if statement

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,20 +5,20 @@ public class SelectionSort implements SortingAlgorithm {
       swap(arr, i, findSmallest(arr, i));
     }
   }
-  
+
   // Returns the index of the smallest value in the array
   private int findSmallest(int[] arr, int starting) {
     int smallest = starting;
-    
+
     for (int i = starting + 1; i < arr.length; i++) {
-      if (arr[smallest] < arr[i]) {
+      if (arr[smallest] > arr[i]) {
         smallest = i;
       }
     }
-    
+
     return smallest;
   }
-  
+
   // Swaps the values of two indexes of an array
   private void swap(int[] arr, int before, int after) {
     int temp = arr[before];
